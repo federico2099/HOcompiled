9311b0aaee910921949e9dedc902db65e6c47e13
Federico Hernandez <federico2099@gmail.com>
2018-02-28 20:09:48 -0300

    Comente el c-sum.c

--- multilenguaje/04-c-from-f/c-sum.c
+++ multilenguaje/04-c-from-f/c-sum.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include "c-sum.h"
 
-void sum_abs_(int *in, int *num, int *sum) {
-   int i;
-
-   for (i=0,*sum=0; i < *num; ++i) {
+void sum_abs_(int *in, int *num, int *sum) { //Aca se cambia int por void para definirla como 
+   int i;                                    //vacia, de esta manera no tiene que "returnear"
+                                             // nada y no me arma lio con FORTRAN que no acepta
+   for (i=0,*sum=0; i < *num; ++i) {         // returns
        *sum += abs(in[i]);
    }
-//   return ;
+//   return ; //Por lo que puse arriba es que esta comentado este return
 }
  
