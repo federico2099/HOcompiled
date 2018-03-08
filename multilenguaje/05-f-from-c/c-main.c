204b6edf6e39d70e93068db3126b7883ea131e5f
Federico <federico2099@gmail.com>
2018-03-07 21:02:27 -0300

    subido 07-03

--- multilenguaje/05-f-from-c/c-main.c
+++ multilenguaje/05-f-from-c/c-main.c
@@ -1,18 +1,22 @@
 
 #include <stdio.h>
-#include "c-sum.h"
 
 #define NUM 200
 
+extern void sum_abs_(int *data, int *num, int *sumat);
 int main(int argc, char **argv)
 {
-    int data[NUM], num, i;
+    int data[NUM], num, i, sumat;
 
     num = NUM;
     for (i=0; i < num; i++) {
         data[i] = i - 100 + 1;  /* NOTE: difference in array numbering */  
     }
 
-    printf("sum=%d\n", sum_abs(data, num));
-    return 0;
-}
+    sum_abs_(data, &num, &sumat); /* saque el llamado a sumat_abs afuera del print */
+    printf("sum=%d\n", sumat);    /* porque FORTRAN "devuelve el puntero donde esta */ 
+    return 0;                     /* el valor de sumat. Entonces tengo que pedir en el */
+}                                 /* printf que imprima el valor que se encuentra dentro */
+                                  /* del puntero que me devolvio FORTRAN y eso justamente */
+                                  /* es sumat. Si no hago eso y dejo el llamado dentro del */
+                                  /* print como estaba, C no sabe que imprimir */
